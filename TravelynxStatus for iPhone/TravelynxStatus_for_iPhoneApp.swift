1506cea2883fb97fee4c99656f6987efdc6a0359
Lola <mail@1lola.me>
2023-08-16 08:07:21 +0200

    Initial Commit

---
+++ TravelynxStatus for iPhone/TravelynxStatus_for_iPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelynxStatus_for_iPhoneApp.swift
+//  TravelynxStatus for iPhone
+//
+//  Created by Lola Schwan on 16.08.23.
+//
+
+import SwiftUI
+
+@main
+struct TravelynxStatus_for_iPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
